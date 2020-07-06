5587c88fcac903915060bec0ee1c47d265a55bfa
Combi2k2 <40012077+Combi2k2@users.noreply.github.com>
2020-07-06 09:02:25 +0700

    Update DynamicConnectivity.cpp

--- DataStructure/DynamicConnectivity.cpp
+++ DataStructure/DynamicConnectivity.cpp
@@ -1,125 +1,139 @@
-//Problem: 813F
+//Problem: 100551A
 //This is just basic implementation of this type
 
 #include<bits/stdc++.h>
 
 using namespace std;
 
-#define ll  long long
-#define ld  double
-
-#define sz(x)   (int)x.size()
-#define all(x)  x.begin(),x.end()
-
-#define pb  emplace_back
-#define X   first
-#define Y   second
-
-const int   N   = 1e5 + 5;
-
-typedef pair<int,int>   ii;
-
-int p[N];
-int s[N];
-int f[N];
-int flag;
-
-ii  lead(int x) {
-    int v = 0;
-    while (x != p[x])   {
-        v ^= f[x];
-        x  = p[x];
-    }
-    return  ii(x,v);
-}
-
-stack<pair<int*,int> >  ops;
-
-void app(int*a,int v)   {   ops.push(make_pair(a,*a));  *a = v; }
-void add(int x,int y)   {
-    ii  pa = lead(x);
-    ii  pb = lead(y);
-
-    if (pa.X == pb.X)   {
-        if (pa.Y == pb.Y)
-            app(&flag,1);
-        return;
-    }
-    if (s[pa.X] < s[pb.X])
-        swap(pa,pb);
-
-    app(p + pb.X,pa.X);
-    app(f + pb.X,pa.Y ^ pb.Y ^ 1);
-    app(s + pa.X,s[pa.X] + s[pb.X]);
-}
-
-vector<ii>  Que[N << 2];
-
+#define pb      push_back
+#define X       first
+#define Y       second
 #define lch     i << 1
 #define rch     i << 1 | 1
 
-void add(int i,int l,int r,int L,int R,ii  v)   {
-    if (l > R)  return;
-    if (L > r)  return;
-    if (L <= l && r <= R)   {
-        Que[i].pb(v);
-        return;
-    }
-    int m = (l + r) / 2;
+const int   N   = 3e5 + 1;
 
-    add(lch,l,m,L,R,v); ++m;
-    add(rch,m,r,L,R,v);
-}
-void cal(int i,int l,int r) {
-    int len = ops.size();
+typedef pair<int,int>   ii;
 
-    for(ii  E : Que[i])
-        add(E.X,E.Y);
-    
-    if (l == r) {
-        if (flag)   cout << "NO\n";
-        else        cout << "YES\n";
+namespace   DCP {   //Dynamic Connectivity Problem
+    struct  PSU {   //Persistent DSU
+        int u, hu;
+        int v, hv;
+    };
+    struct  Que {
+        int u, v;
+        int United;
+        Que(int _u,int _v) : u(_u), v(_v), United(0) {}
+    };
+    int p[N], h[N];
+    int cntComp;
+    stack<PSU> ops;
+
+    int init(int n) {
+        iota(p + 1,p + 1 + n,1);
+        cntComp = n;
     }
-    else    {
+    int lead(int x) {
+        for(; x != p[x] ; x = p[x]);
+        return  x;
+    }
+    int join(int x,int y)   {
+        x = lead(x);
+        y = lead(y);
+        if (x == y)
+            return  0;
+        if (h[x] < h[y])
+            swap(x,y);
+        ops.push({x,h[x],y,h[y]});
+        cntComp--;
+        p[y] = x;
+        if (h[x] == h[y])
+            h[x]++;
+        return  1;
+    }
+    void rollback() {
+        if (ops.empty()) return;
+        h[ops.top().u] = ops.top().hu;
+        p[ops.top().v] = ops.top().v;
+        cntComp++;
+        ops.pop();
+    }
+
+    vector<Que> Q[N << 2];
+    vector<int> ans;
+
+    void add(int i,int l,int r,int L,int R,Que x)   {
+        if (l > R)  return;
+        if (L > r)  return;
+        if (L <= l && r <= R)   {
+            Q[i].pb(x);
+            return;
+        }
         int m = (l + r) / 2;
-        
-        cal(lch,l,m);   ++m;
-        cal(rch,m,r);
+        add(lch,l,m,L,R,x); ++m;
+        add(rch,m,r,L,R,x);
     }
-    while (sz(ops) > len)   {
-        *ops.top().X = ops.top().Y;
-         ops.pop();
+    void cal(int i,int l,int r) {
+        for(Que &t : Q[i])
+            t.United = join(t.u,t.v);
+        if (l == r) ans.pb(cntComp);
+        else    {
+            int m = (l + r) / 2;
+            cal(lch,l,m);   ++m;
+            cal(rch,m,r);
+        }
+
+        reverse(Q[i].begin(),Q[i].end());
+        for(Que &t : Q[i])  if (t.United)
+            rollback();
     }
-}
+};
+using DCP::ans;
+using DCP::add;
+
+bool ask[N];
 
 int main()  {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
+    freopen("connect.in","r",stdin);
+    freopen("connect.out","w",stdout);
+
     int n;  cin >> n;
     int q;  cin >> q;
 
-    map<ii,int> mp;
+    if (q == 0) return  0;
+
+    map<ii,int> Open;
 
     for(int i = 1 ; i <= q ; ++i)   {
-        int x;  cin >> x;
-        int y;  cin >> y;
+        char c; cin >> c;
+        if (c == '?')   {
+            ask[i] = 1;
+            continue;
+        }
+        ii  e;  cin >> e.X >> e.Y;
 
-        if (x > y)  swap(x,y);
+        if (e.X > e.Y)
+            swap(e.X,e.Y);
 
-        ii  e(x,y);
-
-        if(!mp.count(e))    mp[e] = i;
-        else    {
-            add(1,1,q,mp[e],i - 1,e);
-            mp.erase(e);
+        if (c == '+')   Open[e] = i;
+        if (c == '-')   {
+            add(1,1,q,Open[e],i,DCP::Que(e.X,e.Y));
+            Open.erase(e);
         }
     }
-    for(auto it : mp)
-        add(1,1,q,it.Y,q,it.X);
-    
-    iota(p + 1,p + 1 + n,1);
-    fill(s + 1,s + 1 + n,1);
+    for(auto it : Open) {
+        int x = it.X.X;
+        int y = it.X.Y;
+        int l = it.Y;
+        add(1,1,q,l,q,DCP::Que(x,y));
+    }
 
-    cal(1,1,q);
+    DCP::init(n);
+    DCP::cal(1,1,q);
+
+    for(int i = 1 ; i <= q ; ++i)   if (ask[i])
+        cout << ans[i - 1] << "\n";
 }
